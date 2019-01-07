12d55251f60f5d0922114765ef2278ba4815c414
Brad Saund <brad.saund@gmail.com>
2019-01-07 15:03:24 -0500

    starting MCTS

---
+++ src/wip_mcts.cpp
@@ -0,0 +1,32 @@
+#include "mcts.hpp"
+
+
+
+void makeTree()
+{
+    MCTS::Tree tree;
+
+    for(int i=0; i<1000000; i++)
+    {
+        tree.addNode(tree.root);
+    }
+    
+    // std::string unused;
+    // std::cout << "Waiting for user input\n";
+    // std::getline(std::cin, unused);
+
+}
+
+int main()
+{
+
+    for(int i=0; i<1000000; i++)
+    {
+        makeTree();
+    }
+    
+    std::string unused;
+    std::cout << "Waiting for user input\n";
+    std::getline(std::cin, unused);
+    
+}
